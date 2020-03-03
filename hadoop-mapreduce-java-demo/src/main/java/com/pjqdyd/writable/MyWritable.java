7ab4d04d1a3d82e02dd0747d54b967d316f4dca3
pjqdyd <1909025079@qq.com>
2020-03-03 11:02:07 +0800

    自定义数据类型

---
+++ hadoop-mapreduce-java-demo/src/main/java/com/pjqdyd/writable/MyWritable.java
@@ -0,0 +1,63 @@
+package com.pjqdyd.writable;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**   
+ * @Description:  [自定义数据类型]
+ * 实现Writable 不能排序
+ * 实现WritableComparable 可排序
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+public class MyWritable implements WritableComparable<MyWritable> {
+    public String word;
+    public int counter;
+
+    public MyWritable(){}
+    public MyWritable(String word, int counter) {
+        this.word = word;
+        this.counter = counter;
+    }
+
+    public int compareTo(MyWritable myWritable) {
+        return 0;
+    }
+
+    //序列化
+    public void readFields(DataInput dataInput) throws IOException {
+        this.word = dataInput.readUTF();
+        this.counter = dataInput.readInt();
+    }
+
+    //反序列化
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(this.word);
+        dataOutput.writeInt(this.counter);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + word + "\t" + counter + "]";
+    }
+}
